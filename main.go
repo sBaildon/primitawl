10ef64e3ffbf68662e4d18ac3a8cd6cf3b3bd6ae
Sean Baildon <sean@baildon.co>
2017-09-18 10:44:03 +0100

    something kinda working

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+/* Prefer to make this const */
+var (
+	depth *int = flag.Int("depth", 2, "Max crawl depth")
+	followExternal *bool = flag.Bool("follow-external", false, "Follow external links")
+	root string
+)
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		fmt.Println("Args != 1")
+		os.Exit(1)
+	}
+	root := flag.Arg(0)
+
+	url, err := url.ParseRequestURI(root)
+	if err != nil {
+		fmt.Printf("Input was not a valid URL: %s\n", root)
+		os.Exit(1)
+	}
+
+	BeginCrawl(*url, *depth)
+}
+
+func BeginCrawl(u url.URL, maxDepth int) {
+	Crawl(u, 0, maxDepth)
+}
+
+func Crawl(u url.URL, depth int, maxDepth int) {
+	if maxDepth == depth {
+		fmt.Println("Reached max depth")
+		return
+	}
+
+	fmt.Printf("Crawling %s\n", u.String())
+
+	resp, err := http.Get(u.String())
+	if err != nil {
+		panic(err)
+	}
+
+	z := html.NewTokenizer(resp.Body)
+
+	depth++
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			return // End of the document, we're done
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			if t.Data == "a" {
+				for _, a := range t.Attr {
+					if a.Key == "href" {
+						_u, err := url.ParseRequestURI(a.Val)
+						if err != nil {
+							fmt.Println("Not a real URL")
+							return
+						}
+
+						fmt.Printf("[%s]\t%s\t %s\n", _u.String(), _u.Host, u.Host)
+						if _u.Host == u.Host {
+							Crawl(*_u, depth, maxDepth)
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func shouldVisit(u url.URL) bool {
+	return ( (*followExternal) && (u.Hostname() == root) )
+}
+
+func usage() {
+	fmt.Printf("Usage: %s <url>", os.Args[0])
+}
